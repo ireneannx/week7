6d062d4ffe4827b47c8d997af55c6a3f2b7519d8
ireneannx <irene2104@gmail.com>
2019-08-30 13:52:52 +0530

    reformated NavChange.js

--- intro2/intro2/src/NavChange.js
+++ intro2/intro2/src/NavChange.js
@@ -1,5 +1,19 @@
 import React, { Component } from 'react';
 
+const LangSelect = (props) => {
+  //instead of sending props, we can also do ({selectedLanguage, updateLanguage})
+  const languages = ['All', "JavaScript", "Python", "Ruby"];
+  console.log(props)
+  return (
+    <div style={{ display: "flex" }}>
+      {languages.map((lang) => <li key={lang} style={props.selectedLanguage === lang ? { color: "#FF0000" } : null} onClick={() => props.updateLanguage(lang)}>{lang}</li>)
+      }
+    </div>
+  )
+}
+
+
+
 class Nav extends Component {
   state = {
     selectedLanguage: "All"
@@ -12,13 +26,10 @@ class Nav extends Component {
   }
 
   render() {
-    const languages = ['All', "JavaScript", "Python", "Ruby"];
+
     const { selectedLanguage } = this.state
     return (
-      <div>
-        {languages.map((lang) => <li key={lang} style={selectedLanguage === lang ? { color: "#FF0000" } : null} onClick={() => this.updateLanguage(lang)}>{lang}</li>)
-        }
-      </div>
+      <LangSelect selectedLanguage={selectedLanguage} updateLanguage={this.updateLanguage} />
     );
   }
 }
